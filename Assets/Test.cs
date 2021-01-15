dde2d46a10ca4f8084cd9b56eeec58d38497966f
gltovar <louis@kactuskrunch.com>
2021-01-14 19:46:33 -0800

    initial commit of unity project files

---
+++ Assets/Test.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Stateless;
+
+public class Test : MonoBehaviour
+{
+    enum State
+    {
+        Init,
+        Begin,
+        Middle,
+        End
+    }
+
+    enum Trigger
+    {
+        NEXT,
+        RESET
+    }
+
+    StateMachine<State, Trigger> machine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        machine = new StateMachine<State, Trigger>(State.Init);
+
+        machine.Configure(State.Init)
+            
+            .Permit(Trigger.NEXT, State.Begin);
+
+        machine.Configure(State.Begin)
+            .OnEntry(BeginState)
+            .Permit(Trigger.NEXT, State.Middle);
+
+        machine.Configure(State.Middle)
+            .OnEntry(MiddleState)
+            .Permit(Trigger.NEXT, State.End);
+
+        machine.Configure(State.End)
+            .OnEntry(EndState)
+            .Permit(Trigger.RESET, State.Begin);
+
+        machine.Fire(Trigger.NEXT);
+    }
+
+    void BeginState()
+    {
+        Debug.Log("Begin");
+    }
+
+    void MiddleState()
+    {
+        Debug.Log("Middle");
+    }
+
+    void EndState()
+    {
+        Debug.Log("End");
+    }
+
+    float stateDelay = 5f;
+    float currentDelay = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        currentDelay -= Time.deltaTime;
+        if(currentDelay <= 0)
+        {
+            currentDelay = stateDelay;
+            if (machine.CanFire(Trigger.NEXT))
+            {
+                machine.Fire(Trigger.NEXT);
+            }
+            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Stateless-Unity
 Compiled version of Stateless compatible with Unity Package Manager
+
+Learn more about [Stateless](https://github.com/dotnet-state-machine/stateless)
+
+## Installation
+
+Add the following to the Unity Package Manager or project manafest ```https://github.com/gltovar/Stateless-Unity.git#UPM```
+
+Can substitute ```#UPM``` with any of the version numbers below. EX: ```#4.4.0```
+
+## Suported Versions
+
+- 4.4.0
